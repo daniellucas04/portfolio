5b6e0aa03b906ef29933ef59aa1c912b249c9c4c
daniel <daniellucas04>
2023-07-21 22:01:53 -0300

    feat: adding button 'see more' on projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,3 +1,5 @@
+import { Eye } from "lucide-react";
+
 export default function Projects(){
     return(
         <div className="mt-14">
@@ -5,7 +7,7 @@ export default function Projects(){
                 <h1 className="text-3xl">Projects</h1>
                 <div className="flex justify-center">
                     <div className="mt-4 text-center">
-                        <a href="https://github.com/daniellucas04/nextjs-cs-source-mods" target="_blank" className="text-2xl font-light hover:text-zinc-600 hover:underline hover:underline-offset-auto">Games modifications</a>
+                        <a href="https://github.com/daniellucas04/nextjs-cs-source-mods" target="_blank" className="transition-colors text-2xl font-light bg-zinc-800 text-zinc-50 hover:bg-zinc-200 hover:text-zinc-800">Mods application</a>
                         <div className="border-2 shadow-xl mt-2 border-zinc-400 rounded-md overflow-hidden mb-10">
                             <img 
                                 src="https://user-images.githubusercontent.com/97129532/254929601-95af1bbc-1056-4784-b01c-cad7ddbe59ae.gif" 
@@ -16,8 +18,8 @@ export default function Projects(){
                     </div>
                 </div>
                 <div className="flex gap-5 justify-center">
-                    <section>
-                        <a href="https://github.com/daniellucas04/first-flutter-app" target="_blank" className="text-2xl font-light hover:text-zinc-600 hover:underline hover:underline-offset-auto">Flutter application</a>
+                    <section className="text-center">
+                        <a href="https://github.com/daniellucas04/first-flutter-app" target="_blank" className="transition-colors text-2xl font-light bg-zinc-800 text-zinc-50 hover:bg-zinc-200 hover:text-zinc-800">Flutter application</a>
                         <div className="border-2 shadow-xl mt-2 border-zinc-400 rounded-md overflow-hidden mb-10">
                             <img 
                                 src="https://user-images.githubusercontent.com/97129532/239603638-a420bbab-c3bd-4b51-b3f5-ca92ec3a4022.gif" 
@@ -26,8 +28,8 @@ export default function Projects(){
                             />
                         </div>
                     </section>
-                    <section>
-                        <a href="" target="_blank" className="text-2xl font-light hover:text-zinc-600 hover:underline hover:underline-offset-auto">Flutter app (Google Codelabs)</a>
+                    <section className="text-center"> 
+                        <a href="" target="_blank" className="transition-colors text-2xl font-light bg-zinc-800 text-zinc-50 hover:bg-zinc-200 hover:text-zinc-800">Another project</a>
                         <div className="border-2 shadow-xl mt-2 border-zinc-400 rounded-md overflow-hidden mb-10">
                             <img 
                                 src="https://user-images.githubusercontent.com/97129532/239603638-a420bbab-c3bd-4b51-b3f5-ca92ec3a4022.gif" 
@@ -37,6 +39,10 @@ export default function Projects(){
                         </div>
                     </section>
                 </div>
+                <a href="https://github.com/daniellucas04" target="_blank" className="bg-zinc-800 text-zinc-50 p-1 rounded-sm text-xl hover:bg-zinc-700 transition-colors flex gap-1 w-32 items-center justify-center">
+                    <Eye size={20} />
+                    See more
+                </a>
             </div>
         </div>
     )
--- src/components/tech.tsx
+++ src/components/tech.tsx
@@ -4,9 +4,9 @@ export default function Tech(){
             <h1 className="text-3xl">Technologies</h1>
             <div className="ml-8">
                 <p>This section is going to show you what technologies I'm currently using</p>
-                <div className="flex justify-evenly mt-8 mr-32">
+                <div className="grid grid-cols-2 mt-8 mr-32 gap-4">
                     <section>
-                        <h1 className="text-lg font-semibold">Programming Languages</h1>
+                        <span className="bg-zinc-800 text-zinc-50 px-1 ml-1 text-lg">Programming languages</span> 
                         <ul className="list-decimal ml-6">
                             <li>C/C++</li>
                             <li>C#</li>
@@ -16,21 +16,7 @@ export default function Tech(){
                         </ul>
                     </section>
                     <section>
-                        <h1 className="text-lg font-semibold">Database</h1>
-                        <ul className="list-decimal ml-6">
-                            <li>SQL/SQLite</li>
-                            <li>Sequelize</li>
-                        </ul>
-                    </section>
-                    <section>
-                        <h1 className="text-lg font-semibold">Network</h1>
-                        <ul className="list-decimal ml-6">
-                            <li>Linux</li>
-                            <li>Windows Server</li>
-                        </ul>
-                    </section>
-                    <section>
-                        <h1 className="text-lg font-semibold">Frameworks</h1>
+                        <span className="bg-zinc-800 text-zinc-50 px-1 ml-1 text-lg">Frameworks</span> 
                         <ul className="list-decimal ml-6">
                             <li>React</li>
                             <li>Angular</li>
@@ -38,6 +24,21 @@ export default function Tech(){
                             <li>Bootstrap</li>
                         </ul>
                     </section>
+                    <section>
+                        <span className="bg-zinc-800 text-zinc-50 px-1 ml-1 text-lg">Database</span> 
+                        <ul className="list-decimal ml-6">
+                            <li>SQL/SQLite</li>
+                            <li>Sequelize</li>
+                        </ul>
+                    </section>
+                    <section>
+                        <span className="bg-zinc-800 text-zinc-50 px-1 ml-1 text-lg">Network</span> 
+                        <ul className="list-decimal ml-6">
+                            <li>Linux</li>
+                            <li>Windows Server</li>
+                        </ul>
+                    </section>
+                    
                 </div>
             </div>
         </div>
